fceec2dc6494a8da5d87e13342d97ca2bff468f5
mcapizzi <mcapizzi@email.arizona>
2015-04-09 13:38:43 -0700

    added scalaBreeze to handle cosine similarity

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ name := "Unbound"
 
 version := "1.0"
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.11.5"
 
 javaOptions += "-Xmx6G"
 
@@ -14,11 +14,11 @@ libraryDependencies ++= Seq(
   "edu.arizona.sista" % "processors_2.11" % "5.2",
   "edu.arizona.sista" % "processors_2.11" % "5.2" classifier "models",
   "org.apache.commons" % "commons-math3" % "3.3",
-  //"edu.cmu.cs" % "ark-tweet-nlp" % "0.3.2",
   "org.apache.commons" % "commons-compress" % "1.9",
-  "org.apache.commons" % "commons-io" % "1.3.2"/*,
+  "org.apache.commons" % "commons-io" % "1.3.2",
+  "org.scalanlp" %% "breeze-natives" % "0.11.2",
+  "org.scalanlp" %% "breeze_2.10" % "0.11.2"/*,
+  "edu.cmu.cs" % "ark-tweet-nlp" % "0.3.2",
   "edu.stanford.nlp" % "stanford-corenlp" % "3.5.0",
-  "org.scalanlp" % "breeze-natives" % "0.10",
-  "org.scalanlp" % "breeze_2.10" % "0.10",
   */
 )
--- src/main/scala/TextComplexity/Similarity.scala
+++ src/main/scala/TextComplexity/Similarity.scala
@@ -30,6 +30,7 @@ object Similarity {
     fullDocs.map(_.sentences.map(_.words.toVector)).flatten.toVector                                    //flat list of sentences
   }
 
+  //TODO figure out why it's still capturing punctuation
   //get all words from all texts
       //keepProper = true --> keeps ALL tokens that are not punctuation
       //keepProper = false --> removes punctuation and proper nouns
@@ -60,23 +61,23 @@ object Similarity {
 
   //vectors for countMatrix
   def makeCountMatrixRows(directory: File): Vector[String] = {
-    extractAllWordsAllTexts(rawTextFile, keepProper = false).
+    extractAllWordsAllTexts(directory, keepProper = false).
       filter(word => word._2._2.matches("NN.") || word._2._2.matches("VB.") || word._2._2.matches("JJ.") || word._2._2.matches("RB.")).      //only keep "important" POS
       map(_._1).
-      map(_.toLowerCase)                                                                                                                     //make all lowercase
+      map(_.toLowerCase).distinct                                                                                                                     //make all lowercase
   }
 
   def makeCountMatrixColumns(directory: File): Vector[String] = {
-    extractAllWordsAllTexts(directory, keepProper = true).map(_._1).map(_.toLowerCase)
+    extractAllWordsAllTexts(directory, keepProper = true).map(_._1).map(_.toLowerCase).distinct
   }
-
 
   //map representing countMatrix
   def makeWordMap(allSentencesAllTexts: Vector[Vector[String]], rowMatrix: Vector[String]/*, columnMatrix: Vector[String]*/): Vector[(String, Map[String, Double])] = {
     for (wordToCalculate <- rowMatrix) yield {                                                      //for every word in wordMatrix rows
     val frequency = new Frequency()                                                                       //build frequency to capture counts
-    val containsWord = allSentencesAllTexts.filter(sentence => sentence.contains(wordToCalculate)).       //capture the sentences containing that word
-        map(_.map(_.toLowerCase))                                                                             //make all words lowercase
+    val containsWord = allSentencesAllTexts.
+        map(_.map(_.toLowerCase)).                                                                          //make all words lowercase
+        filter(sentence => sentence.contains(wordToCalculate))                                              //capture the sentences containing that word
       for (sentence <- containsWord) {                                                                      //for each sentence
         for (word <- sentence.filterNot(_ == wordToCalculate)) {                                                //for each word that is not the wordToCalculate
           frequency.addValue(word)                                                                                //increment the count ==> # of times it exists in same sentence as wordToCalculate
@@ -90,6 +91,20 @@ object Similarity {
     }
   }
 
+  //TODO apply breeze to cosine similarity
+  //calculate cosine similarity of two words
+    //Note: both words must appear in rows of wordCountMap
+  /*def calculateWordSimilarity(wordOne: String, wordTwo: String, wordCountMap: Vector[(String, Map[String, Double])]): Double = {
+    val wordOneVector = wordCountMap.filter(_._1 == wordOne).map(_._2.values).flatten
+    val wordTwoVector = wordCountMap.filter(_._1 == wordTwo).map(_._2.values).flatten
+    //consule breeze
+      //https://github.com/scalanlp/breeze/wiki/Quickstart
+      //https://github.com/scalanlp/breeze/wiki/Linear-Algebra-Cheat-Sheet
+      //https://gist.github.com/reuben-sutton/2932974
+  }*/
+
+
+  val test = DenseVector
   /*//all sentences in all texts
   val allSentencesFromAllTexts = extractAllSentencesAllTexts(rawTextFile)
 
