48785a03262b21f18d538fb9bcca47900b18470e
mcapizzi <mcapizzi@email.arizona>
2015-04-15 11:08:53 -0700

    began NaiveBayes

--- src/main/scala/TextComplexity/NaiveBayes.scala
+++ src/main/scala/TextComplexity/NaiveBayes.scala
@@ -5,5 +5,47 @@ package TextComplexity
  */
 class NaiveBayes(trainingData: Vector[TextDocument], testDocument: TextDocument) {
 
+  //total # of documents
+  def trainingDataSize = {
+    trainingData.length.toDouble
+  }
+
+  //all possible classes
+  def possibleClasses = {
+    trainingData.map(_.gradeLevel).distinct
+  }
+
+  //class counts
+  def classCounts = {
+    (for (possibleClass <- this.possibleClasses) yield {
+      possibleClass -> trainingData.count(doc => doc.gradeLevel == possibleClass).toDouble
+    }).toMap
+  }
+
+  //class prior probabilities
+        //number of documents in a class / total number of documents
+  def priorProbabilities = {
+    (for (possibleClass <- this.classCounts.keySet) yield {
+      possibleClass -> classCounts(possibleClass) / this.trainingDataSize
+    }).toMap
+  }
+
+  //extract all vocabulary from all documents in training
+  def extractVocabulary = {
+    trainingData.map(textDocument => textDocument.getWords.map(_.toLowerCase)).flatten.distinct
+  }
+
+  //concatenate all documents for each class into one document
+  def makeDocumentConcatenized = {
+    //
+  }
+
+
+
+
+
+
+
+
 
 }
