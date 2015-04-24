3609634b1644232beea81cbecfa9d30234db38a7
mcapizzi <mcapizzi@email.arizona>
2015-04-24 11:04:13 -0700

    TODO finish leaveOneOut method

---
+++ src/main/scala/TextComplexity/Visualization.scala
@@ -0,0 +1,40 @@
+package TextComplexity
+
+import com.quantifind.charts.highcharts
+import com.quantifind.charts.highcharts.Highchart._
+import com.quantifind.charts.Highcharts._
+
+/**
+ * Created by mcapizzi on 4/24/15.
+ */
+object Visualization {
+
+  //https://github.com/quantifind/wisp
+  //http://api.highcharts.com/highcharts
+
+  //help()
+
+  //graph type
+  //title("blah")
+  //xAxis("blah")
+  //yAxis("blah")
+  //legend(List("blah", "blah"))
+
+  //accuracy pie chart
+  //pie(accuracy, 1 - accuracy)
+
+  //histogram
+  //histogram(distances, distances.distinct.length-1)
+
+  //bar chart
+  //bar(List([all precision scores])
+  //hold
+  //bar(List([all recall scores])
+  //hold
+  //bar(List([all f1 scores])
+  //xAxis("model")
+  //yAxis("score")
+  //legend(List("precision", "recall", "f1"))
+
+
+}
